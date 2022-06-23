a7ddfe994312ae4a4820e312dcaeba128435d607
Nozimjon <nozimjonjuraev3003@gmail.com>
2022-06-23 22:13:49 +0500

    FEATURE: add stats

--- src/views/TopicsIndex.vue
+++ src/views/TopicsIndex.vue
@@ -10,15 +10,37 @@
       </div>
     </div>
   </div>
-  <div class="row" v-else>
-    <div class="col-12 col-md-4 mb-4" v-for="topic in topics" :key="topic.slug">
-      <div class="card" style="width: 18rem">
-        <div class="card-body">
-          <h5 class="card-title">{{ topic.title }}</h5>
-          <p class="text-secondary mb-0">{{ topic.count }} вопросов</p>
-          <router-link :to="{ name: 'topic-detail', params: { id: topic.slug } }" class="card-link">
-            Пройти тест
-          </router-link>
+  <div v-else>
+    <section class="text-center">
+      <div class="max-w-screen-xl px-4 py-16 mx-auto sm:px-6 lg:px-8">
+        <ul class="grid grid-cols-2 gap-4 border-2 border-teal-600 rounded-xl lg:grid-cols-3">
+          <li class="p-8">
+            <p class="text-2xl font-extrabold text-teal-500">2,101</p>
+            <p class="mt-1 text-lg font-medium">Пройдено тестов</p>
+          </li>
+
+          <li class="p-8">
+            <p class="text-2xl font-extrabold text-teal-500">16</p>
+            <p class="mt-1 text-lg font-medium">Среднее кол-во вопросов в тесте</p>
+          </li>
+
+          <li class="p-8">
+            <p class="text-2xl font-extrabold text-teal-500">1500</p>
+            <p class="mt-1 text-lg font-medium">Кол-во успешных тестов</p>
+          </li>
+        </ul>
+      </div>
+    </section>
+    <div class="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 max-w-screen-xl mx-auto">
+      <div class="" v-for="topic in topics" :key="topic.slug">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">{{ topic.title }}</h5>
+            <p class="text-secondary mb-0">{{ topic.count }} вопросов</p>
+            <router-link :to="{ name: 'topic-detail', params: { id: topic.slug } }" class="card-link">
+              Пройти тест
+            </router-link>
+          </div>
         </div>
       </div>
     </div>
